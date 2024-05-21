3b4b3702b4bb587a86bf65de646d90d38f2debb4
Simon Guest <simon.guest@tesujimath.org>
2024-05-21 17:13:55 +1200

    Make test filenames unique on case-insensitive filesystems

--- tests/path_ext.rs
+++ tests/path_ext.rs
@@ -18,22 +18,22 @@ use walkdir::WalkDir;
 
 // Naming for files and directories in the link farms is as follows:
 // - directories are capitalised
-// - files are lower-cased
+// - files are lower-cased and have a numeric suffix to avoid case insensitivity issues on Windows
 // - relative symlinks have an underscore prefix
 // - absolute symlinks have an equals prefix
 
-#[test_case("x", ".")]
+#[test_case("x1", ".")]
 #[test_case("A", ".")]
-#[test_case("A/a", "A")]
-#[test_case("A/B/b", "A/B")]
+#[test_case("A/a1", "A")]
+#[test_case("A/B/b1", "A/B")]
 #[test_case("A/B/C", "A/B")]
 #[test_case("A/B/C/..", "A/B/C/../..")]
 #[test_case("A/B/C/.", "A/B"; "trailing dot is ignored")]
 #[test_case("A/./B/C", "A/./B"; "intermediate dot remains")]
 #[test_case("A/../A/B/C", "A/../A/B"; "intermediate dotdot remains")]
 #[test_case("A/.D", "A" ; "hidden directory")]
-#[test_case("A/.D/d", "A/.D" ; "file in hidden directory")]
-#[test_case("A/.D/.d", "A/.D" ; "hidden file in hidden directory")]
+#[test_case("A/.D/d1", "A/.D" ; "file in hidden directory")]
+#[test_case("A/.D/.d1", "A/.D" ; "hidden file in hidden directory")]
 #[test_case("", ".."; "empty path")]
 #[test_case(".", ".."; "bare dot")]
 #[test_case("..", "../.."; "bare dotdot")]
@@ -41,15 +41,15 @@ use walkdir::WalkDir;
 fn test_real_parent_files_directories(path: &str, expected: &str) {
     let farm = LinkFarm::new();
 
-    farm.file("x")
+    farm.file("x1")
         .dir("A")
         .dir("A/B")
         .dir("A/B/C")
         .dir("A/.D")
-        .file("A/a")
-        .file("A/B/b")
-        .file("A/.D/d")
-        .file("A/.D/.d");
+        .file("A/a1")
+        .file("A/B/b1")
+        .file("A/.D/d1")
+        .file("A/.D/.d1");
 
     check_real_parent_ok(&farm, path, expected);
 }
@@ -57,7 +57,7 @@ fn test_real_parent_files_directories(path: &str, expected: &str) {
 #[test_case("A/B/_b", "A/B")]
 #[test_case("A/B/_a", "A")]
 #[test_case("A/B/C/_a", "A")]
-#[test_case("_B/b", "_B")]
+#[test_case("_B/b1", "_B")]
 #[test_case("A/_dot", "..")]
 #[test_case("A/B/_A", ".")]
 #[test_case("A/B/_B", "A")]
@@ -67,20 +67,20 @@ fn test_real_parent_files_directories(path: &str, expected: &str) {
 fn test_real_parent_rel_symlinks(path: &str, expected: &str) {
     let farm = LinkFarm::new();
 
-    farm.file("x")
+    farm.file("x1")
         .dir("A")
         .dir("A/B")
         .dir("A/B/C")
-        .file("A/a")
-        .file("A/B/b")
-        .symlink_rel("_x", "x")
+        .file("A/a1")
+        .file("A/B/b1")
+        .symlink_rel("_x", "x1")
         .symlink_rel("_B", "A/B")
         .symlink_rel("A/_dot", "..")
         .symlink_rel("A/B/_A", "..")
         .symlink_rel("A/B/_B", ".")
-        .symlink_rel("A/B/_b", "b")
-        .symlink_rel("A/B/_a", "../a")
-        .symlink_rel("A/B/C/_a", "../../a");
+        .symlink_rel("A/B/_b", "b1")
+        .symlink_rel("A/B/_a", "../a1")
+        .symlink_rel("A/B/C/_a", "../../a1");
 
     check_real_parent_ok(&farm, path, expected);
 }
@@ -94,12 +94,12 @@ fn test_real_parent_rel_indirect_symlinks(path: &str, expected: &str) {
     farm.dir("A")
         .dir("A/B")
         .dir("A/B/C")
-        .file("A/B/b")
-        .file("A/B/C/c")
+        .file("A/B/b1")
+        .file("A/B/C/c1")
         .symlink_rel("_B", "A/B")
-        .symlink_rel("A/B/C/_b", "../../../_B/b")
+        .symlink_rel("A/B/C/_b", "../../../_B/b1")
         .symlink_rel("__b", "A/B/C/_b")
-        .symlink_rel("_c", "A/B/C/c")
+        .symlink_rel("_c", "A/B/C/c1")
         .symlink_rel("A/B/__c", "../../_c");
 
     check_real_parent_ok(&farm, path, expected);
@@ -114,10 +114,10 @@ fn test_real_parent_abs_symlinks(path: &str, expected: &str) {
     farm.dir("A")
         .dir("A/B")
         .dir("A/C")
-        .file("A/B/b")
-        .file("A/a")
-        .symlink_abs("A/B/_b", "A/B/b")
-        .symlink_abs("A/B/_a", "A/a")
+        .file("A/B/b1")
+        .file("A/a1")
+        .symlink_abs("A/B/_b", "A/B/b1")
+        .symlink_abs("A/B/_a", "A/a1")
         .symlink_abs("A/B/_C", "A/C");
 
     check_real_parent_ok(&farm, path, farm.absolute(expected));
@@ -142,8 +142,8 @@ fn test_real_parent_symlink_cycle_error(path: &str) {
 }
 
 #[test_case("X")]
-#[test_case("X/y")]
-#[test_case("A/y")]
+#[test_case("X/y1")]
+#[test_case("A/y1")]
 #[test_case("_a")]
 #[test_case("_b")]
 fn test_real_parent_io_error(path: &str) {
@@ -151,9 +151,9 @@ fn test_real_parent_io_error(path: &str) {
 
     farm.dir("A")
         .dir("A/B")
-        .file("A/B/b")
-        .symlink_rel("_a", "A/a")
-        .symlink_rel("_b", "A/B/C/b");
+        .file("A/B/b1")
+        .symlink_rel("_a", "A/a1")
+        .symlink_rel("_b", "A/B/C/b1");
 
     check_real_parent_err(&farm, path, ErrorKind::Io);
 }
@@ -165,10 +165,10 @@ fn test_real_parent_symlink_cycle_look_alikes(path: &str, expected: &str) {
     farm.dir("A")
         .dir("A/A")
         .dir("A/A/A")
-        .file("A/A/A/a")
+        .file("A/A/A/a1")
         .symlink_rel("_a", "A/_a")
         .symlink_rel("A/_a", "A/_a")
-        .symlink_rel("A/A/_a", "A/a");
+        .symlink_rel("A/A/_a", "A/a1");
 
     check_real_parent_ok(&farm, path, expected);
 }
